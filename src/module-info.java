a4f09fbcd877072044b9f4286a59e47562f9624a
SviatoslavBalandin <sviatoslavbalandin@gmail.com>
2020-03-10 13:32:26 +0100

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module Org.openjfx {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    opens sample;
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextArea idMainTextArea;
+
+    @FXML
+    public TextField idTextField;
+}
