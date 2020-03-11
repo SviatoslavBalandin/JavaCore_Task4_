110336f42fbd28383eac28af0b83b76c19c82736
SviatoslavBalandin <sviatoslavbalandin@gmail.com>
2020-03-11 12:18:29 +0100

    final commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -10,4 +12,18 @@ public class Controller {
 
     @FXML
     public TextField idTextField;
+
+    public void btnSend() {
+        pushTextToMainArea();
+    }
+
+    @FXML
+    public void onEnter(ActionEvent event) {
+        pushTextToMainArea();
+    }
+
+    private void pushTextToMainArea () {
+        idMainTextArea.appendText(idTextField.getText() + "\n");
+        idTextField.clear();
+    }
 }
